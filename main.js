73e07a7e3c8e2f77dd57d8db54d74533d75cb630
rana zaid <itszaidrajpoot@gmail.com>
2024-03-24 03:49:07 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+// 6-Can you print the first repeating character from the string
+
+// let str = "hello there";
+
+// for(let i = 0;i<str.length-1;i++){
+//     for(let j = 1;j<str.length;j++){
+//         if(str[i]==str[i+j]){
+//             console.log(str[i])
+//         }
+//     }
+// }
