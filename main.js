62e87fc276043b775a8c38d29bee01712f09f119
rana zaid <itszaidrajpoot@gmail.com>
2024-03-25 01:37:55 +0500

    add some code

--- main.js
+++ main.js
@@ -9,3 +9,18 @@
 //         }
 //     }
 // }
+
+
+// 2-swap two variables without the need for temporary third variable 
+
+
+
+// let numbers = [43, 23, 54, 25, 61];
+
+// console.log(`Before Swapping: ${numbers}`);
+
+// [numbers[2], numbers[3]] = [numbers[3], numbers[2]]  //by destructing method
+
+// console.log(`After Swapping: ${numbers}`)
+
+
